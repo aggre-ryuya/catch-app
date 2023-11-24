422d3f431297ca0d3b026b970c83bf9bbe2c1935
aggre-ryuya <shimoji.r@aggr.jp>
2023-11-24 23:04:19 +0900

    データベースファイル設定

--- database/migrations/2023_11_24_213823_create_stores_table.php
+++ database/migrations/2023_11_24_213823_create_stores_table.php
@@ -12,8 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('stores', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id()->comment('Id');
+            $table->string('store_name',10)->comment('店名');
+            $table->enum('deleted_at',[1,0])->comment('論理削除');
+            $table->timestamp('created_at')->comment('作成日時');
+            $table->timestamp('updated_at')->comment('更新日時');
         });
     }
 
