8566c3c45105b90cf6a397a2d1d24fb179791a76
aggre-ryuya <shimoji.r@aggr.jp>
2023-11-25 12:45:43 +0900

    修正

--- database/migrations/2023_11_24_204433_update_users_table.php
+++ database/migrations/2023_11_24_204433_update_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable()->comment('メール確認可否')->change();
             $table->string('password', 255)->comment('パスワード')->change();
             $table->enum('role', [0, 1])->default(0)->nullable()->comment('権限')->after('password');;
-            $table->enum('deleted_at', [0, 1])->default(0)->nullable()->comment('論理削除')->after('role');;
+            $table->enum('deleted_at', [0, 1])->default(0)->nullable()->comment('論理削除')->after('role');
             $table->rememberToken()->comment('認証トークン')->change();
             $table->timestamp('created_at')->comment('作成日時')->change();
             $table->timestamp('updated_at')->comment('更新日時')->change();
